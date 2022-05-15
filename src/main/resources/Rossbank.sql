6559150eebed4739878500e2b634f0e776872f6a
Seyedrasool Sadrieh <rasool.sadrieh@gmail.com>
2022-05-14 21:20:14 -0700

    back to the older version

--- src/main/resources/Rossbank.sql
+++ src/main/resources/Rossbank.sql
@@ -1,16 +1,12 @@
 
 
-insert into "rossbank".users
+insert into rossbank.users
 values
 (4 ,'Charles', 'Jester', 'cj@ddsdmail.com', 'password', '01-01-0001'),
 (5,'abbas', 'akbari', 'abbas@hhg.com', 'password', '01-01-0001'),
 
-insert into "rossbank".users
+insert into rossbank.users
 values
 (2 ,'Ross', 'Sadrieh', 'ross@gmail.com', 'password', '01-01-0001'),
 select * from rossbank.users ;
-
-INSERT INTO rossbank.users
-(id, fname, lname, email, "password", dob)
-VALUES(0, 'Ross', 'Sadrieh', 'ross@gmail.com', 'password', '02-05-1990');
 
